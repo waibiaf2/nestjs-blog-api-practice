e41589b17f0c8522ca46c97e202963a876caa13d
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-05-13 23:25:26 +0300

    Extend User entity with id and firstName fields

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,14 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class User {}
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 512,
+    nullable: false,
+  })
+  firstName: string;
+}
