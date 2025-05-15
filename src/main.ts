88dd752990e097b1b7e886a19174fd376d275e7c
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-05-15 04:25:40 +0300

    Add Swagger documentation setup and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,6 +19,19 @@ async function bootstrap() {
       },
     }),
   );
+  
+  /**
+   * Swagger Configurations
+   * */
+  const config = new DocumentBuilder()
+    .setTitle('Api Documentation for the Blog Api')
+    .setDescription('This is the api documentation for the blog api')
+    .setVersion('1.0')
+    .addTag('Practice Blog Api')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
