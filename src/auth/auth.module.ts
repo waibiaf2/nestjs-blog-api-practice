fa3c12486a8b956e9dc1de699047b14fec542547
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-05-19 17:23:25 +0300

    Refactor AuthModule to include SignInProvider and HashingProvider with Bcrypt implementation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthService } from './providers/auth.service';
 import { AuthController } from './auth.controller';
+import { SignInProvider } from './providers/sign-in-provider';
+import { UsersModule } from '../users/users.module';
+import { HashingProvider } from './providers/hashing-provider';
+import { BcryptProvider } from './providers/bcrypt-provider';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  providers: [
+    AuthService,
+    SignInProvider,
+    {
+      provide: HashingProvider,
+      useClass: BcryptProvider,
+    },
+    BcryptProvider,
+  ],
+  controllers: [AuthController],
+  imports: [forwardRef(() => UsersModule)],
+  exports: [AuthService, HashingProvider],
 })
 export class AuthModule {}
